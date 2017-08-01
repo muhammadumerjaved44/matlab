ace8f2c9842be53d62576c056ea9e2303301ddeb
Muhammad Umer Javed <muhammadumerjaved44@gmail.com>
2017-08-01 10:37:40 +0500

    chnage the line position

--- currentDateTime.m
+++ currentDateTime.m
@@ -1,6 +1,9 @@
 function dateTime = currentDateTime()
+dateTime = datetime('now','TimeZone','local','Format','eeee, MMMM d, yyyy h:mm a');
 % dateTime = datestr(dateTime, 'mmmm/dd/yyyy HH:MM:SS AM');
 % dateTime = ['' datestr(dateTime, 'mmmm/dd/yyyy HH:MM:SS AM') ''];
+
 % dateTime = datetime('now','TimeZone','local');
-dateTime = datetime('now','TimeZone','local','Format','eeee, MMMM d, yyyy h:mm a');
+
+
 end
